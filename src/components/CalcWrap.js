7bbfa4e43a3cc0e8fe3e09429e5252cc6ff27245
kevinsmasters <gmail@kevinmasters.com>
2022-10-29 11:52:18 -0400

    main layout divs added

---
+++ src/components/CalcWrap.js
@@ -0,0 +1,18 @@
+
+function CalcWrap() {
+    return (
+        <div id="calcBox">
+            <div id="functionBox"></div>
+            <div id="display"></div>
+            <div id="keyPad"></div>
+            <div id="logo">
+                <img 
+                    src="https://static.wikia.nocookie.net/villains/images/4/49/Doofenshmirtzlogo.png" 
+                    alt="Calculatorenator" 
+                />
+            </div>
+        </div>
+    )
+}
+
+export default CalcWrap;
